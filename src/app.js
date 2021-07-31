ca86637db496a3712ef0e9d84d1e12a192f5506a
ChiaraR3 <chiara.r.3@live.it>
2021-07-31 17:24:00 +0000

    errore

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = GetDomain();
+  console.log("Any strings");
 };
+
+function GetDomain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let web = [".com", ".it", ".es"];
+
+  let proAle = Math.floor(Math.random() * pronoun.length);
+  let adjAle = Math.floor(Math.random() * adj.length);
+  let nouAle = Math.floor(Math.random() * noun.length);
+  let webAle = Math.floor(Math.random() * web.length);
+  let finale = [];
+  finale.push(proAle, adjAle, nouAle, webAle);
+
+  return finale;
+  //for (i = 0; i < pronoun.length; i++) {
+  //finale.push(Math.floor(Math.random() * pronoun.length));
+  //}
+  //for (j = 0; i < adj.length; i++) {
+  //finale.push(Math.floor(Math.random() * adj.length));
+  //}
+  //for (x = 0; x < noun.length; x++) {
+  //finale.push(Math.floor(Math.random() * noun.length));
+  //}
+  // for (y = 0; y < web.length; y++) {
+  // finale.push(Math.floor(Math.random() * web.length));
+  //}
+  //return finale;
+}
+
+//console.log(finale);
