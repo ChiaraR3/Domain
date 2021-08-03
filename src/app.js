b802aa485d7154a113fb99b989bac09b57161bd2
ChiaraR3 <chiara.r.3@live.it>
2021-08-03 17:02:49 +0000

    version final

--- src/app.js
+++ src/app.js
@@ -2,23 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function GetDomain() {
+window.onload = function GetDomain(element) {
+  var DomainNames = [];
   let pronouns = ["the", "our"];
   let adjs = ["great", "big"];
   let nouns = ["jogger", "racoon"];
   let webs = [".com", ".it", ".es"];
+  let pro = [];
+  let adj = [];
+  let noun = [];
+  let web = [];
 
-  for (i = 0; i < pronouns.length; i++) {
-    let pronoun = pronouns[i];
+  for (let i = 0; i < pronouns.length; i++) {
+    pro = pronouns[i];
+
+    for (let j = 0; j < adjs.length; j++) {
+      adj = adjs[j];
+
+      for (let x = 0; x < nouns.length; x++) {
+        noun = nouns[x];
+
+        for (let y = 0; y < webs.length; y++) {
+          web = webs[y];
+
+          return [pro + adj + noun + web];
+        }
+      }
+    }
   }
-  for (j = 0; i < adjs.length; i++) {
-    let adj = adjs[j];
-  }
-  for (x = 0; x < nouns.length; x++) {
-    let noun = nouns[x];
-  }
-  for (y = 0; y < webs.length; y++) {
-    let web = webs[y];
-  }
-  console.log(pronoun + adj + noun + web);
 };
