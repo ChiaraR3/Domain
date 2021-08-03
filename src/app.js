c20432cb2f2789c526987db7f55288793eaa9b32
ChiaraR3 <chiara.r.3@live.it>
2021-08-03 16:16:23 +0000

    avance

--- src/app.js
+++ src/app.js
@@ -2,38 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  document.querySelector("#domain").innerHTML = GetDomain();
-  console.log("Any strings");
+window.onload = function GetDomain() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let webs = [".com", ".it", ".es"];
+
+  for (i = 0; i < pronouns.length; i++) {
+    let pronoun = pronouns[i];
+  }
+  for (j = 0; i < adjs.length; i++) {
+    let adj = adjs[j];
+  }
+  for (x = 0; x < nouns.length; x++) {
+    let noun = nouns[x];
+  }
+  for (y = 0; y < webs.length; y++) {
+    let web = webs[y];
+  }
+  console.log(pronoun + adj + noun + web);
 };
-
-function GetDomain() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let web = [".com", ".it", ".es"];
-
-  let proAle = Math.floor(Math.random() * pronoun.length);
-  let adjAle = Math.floor(Math.random() * adj.length);
-  let nouAle = Math.floor(Math.random() * noun.length);
-  let webAle = Math.floor(Math.random() * web.length);
-  let finale = [];
-  finale.push(proAle, adjAle, nouAle, webAle);
-
-  return finale;
-  //for (i = 0; i < pronoun.length; i++) {
-  //finale.push(Math.floor(Math.random() * pronoun.length));
-  //}
-  //for (j = 0; i < adj.length; i++) {
-  //finale.push(Math.floor(Math.random() * adj.length));
-  //}
-  //for (x = 0; x < noun.length; x++) {
-  //finale.push(Math.floor(Math.random() * noun.length));
-  //}
-  // for (y = 0; y < web.length; y++) {
-  // finale.push(Math.floor(Math.random() * web.length));
-  //}
-  //return finale;
-}
-
-//console.log(finale);
